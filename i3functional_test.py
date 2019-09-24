12833a400c9bc531e0617c6dd3cd5ccac5057f56
greatabel <myreceiver2for2github@gmail.com>
2019-09-24 23:35:57 +0800

    i3functional_test.py

--- i3functional_test.py
+++ i3functional_test.py
@@ -7,6 +7,10 @@ class TestApp(unittest.TestCase):
     def test_help(self):
         app = tested_app.test_client()
 
+        hello = app.get('/api')
+
+        body = json.loads(str(hello.data, 'utf8'))
+        self.assertEqual(body['Hello'], 'World!')
 
 if __name__ == '__main__':
     unittest.main()
