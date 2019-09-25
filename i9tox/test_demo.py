1d6bc7f48130b8c366e895b0df88be6977f70c89
greatabel <myreceiver2for2github@gmail.com>
2019-09-26 01:27:02 +0800

    i9tox

---
+++ i9tox/test_demo.py
@@ -0,0 +1,12 @@
+import unittest
+import json
+
+
+
+class TestApp(unittest.TestCase):
+    def test_help(self):
+
+        self.assertEqual('a', 'a')
+
+if __name__ == '__main__':
+    unittest.main()
