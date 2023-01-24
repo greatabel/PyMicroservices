03636064f0ffc1bcb99d70c015fdb86a6776713b
greatabel <myreceiver2for2github@gmail.com>
2023-01-24 23:41:07 +0800

    fix aws 80 port

--- a25066_aws/i1wsgi.py
+++ a25066_aws/i1wsgi.py
@@ -698,4 +698,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-        app.run(host="127.0.0.1", port=5000, threaded=False)
+        app.run(host="127.0.0.1", port=80, threaded=False)
